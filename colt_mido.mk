a7af0b0cc22805172996fbb5db360532c86fa97d
Pratyaksh Bharadwaj <54440925+rockstar5495@users.noreply.github.com>
2021-08-27 09:40:52 +0530

    mido: get rid of fp & desc
    Signed-off-by: rockstar5495 <pratyakshom.05@gmail.com>

--- colt_mido.mk
+++ colt_mido.mk
@@ -42,9 +42,3 @@ TARGET_VENDOR := Xiaomi
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
