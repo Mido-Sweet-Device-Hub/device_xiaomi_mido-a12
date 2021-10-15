59777537d39b15e197c055d372f105e56b25cec5
Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
2021-10-15 19:07:19 +0530

    mido: Add shim for missing symbol in lib-imsvt.so
    * E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: cannot locate symbol "_ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEb" referenced by "/system_ext/lib64/lib-imsvideocodec.so"
    
    Signed-off-by: Palakurthi Sreekanth <palakurthisreekanth995@gmail.com>
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -100,6 +100,9 @@ sed -i -e '$a\\    capabilities NET_BIND_SERVICE' "${DEVICE_BLOB_ROOT}"/vendor/e
 "${PATCHELF}" --add-needed "fakelogprint.so" "${DEVICE_BLOB_ROOT}"/vendor/lib64/hw/fingerprint.goodix.so
 "${PATCHELF}" --add-needed "fakelogprint.so" "${DEVICE_BLOB_ROOT}"/vendor/lib64/hw/gxfingerprint.default.so
 
+# Ims
+"${PATCHELF}" --add-needed "lib-imsvt.so" "${DEVICE_BLOB_ROOT}"/system_ext/lib64/lib-imsvideocodec.so
+
 # Wcnss_service - libqmiservices_shim
 "${PATCHELF}" --add-needed "libqmiservices_shim.so" "${DEVICE_BLOB_ROOT}"/vendor/bin/wcnss_service
 sed -i "s|dms_get_service_object_internal_v01|dms_get_service_object_shimshim_v01|g" "${DEVICE_BLOB_ROOT}"/vendor/bin/wcnss_service
