516f8826fcc35efbc898f8fb7db7988065b85bf0
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-11-16 16:05:12 +0100

    mido: switch to full sepolicy and drop minimal
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,9 +159,9 @@ DISABLE_RILD_OEM_HOOK := true
 VENDOR_SECURITY_PATCH := 2021-02-05
 
 # SELinux
-#include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-#BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-minimal
+include device/qcom/sepolicy-legacy-um/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy-minimal
 
 # Neverallows
 SELINUX_IGNORE_NEVERALLOWS := true
